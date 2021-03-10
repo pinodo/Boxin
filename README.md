8e83a4d49a3d54206d555047a12c5954443ffe64
Alvin <palifarec@gmail.com>
2021-03-10 17:26:52 +0900

    1. Basic Structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boxin
+
+Due to Covid-19, people hesitate to transact the used item in person.
+
+In this vein, I came up with the idea that people can purchase and sell their used item in a designated place, which has a bunch of secure box slots that people can see the items through the window.
+
+Boxin is an indirect transaction management system, which manages the available slots and connects the seller and buyer.
+
+Technologies used: HTML, CSS, JavaScript, NodeJS, git.
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const pageRouter = require("./routes/login-page");
+
+const app = express();
+app.set("port", process.env.PORT || 8001);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
